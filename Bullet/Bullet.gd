281efbfd6b8adbade12685d48d8d11e79ad25091
Wyeth <wyemicha@iu.edu>
2021-02-14 22:00:46 -0500

    completed exercise

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -4,7 +4,15 @@ onready var VP = get_viewport().size
 onready var Explosions = get_node("/root/Game/Explosions")
 var Explosion = load("res://Explosion/Explosion.tscn")
 
+export var Velocity = Vector2(0,-1)
+export var Acceleration = Vector2(0,-1)
 
+
+func _physics_process(_delta):
+	position += Velocity
+	Velocity += Acceleration
+	if global_position.x < -10 or global_position.x > VP.x + 10 or global_position.y < -10 or global_position.y > VP.y + 10:
+		queue_free()
 
 
 
